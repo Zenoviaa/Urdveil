359a9dbecc739100af89ccbc7b5b92ca1292bbfe
Zenovia <calamityjewelstudios@gmail.com>
2024-12-22 22:45:02 -0500

    new item

--- TilesNew/SpringHills/SpringStones.cs
+++ TilesNew/SpringHills/SpringStones.cs
@@ -35,4 +35,31 @@ namespace Urdveil.TilesNew.SpringHills
             //If you need other static defaults it go here
         }
     }
+
+    public class BrokenCarraigeBlock : DecorativeWallItem
+    {
+        public override void SetStaticDefaults()
+        {
+            // Tooltip.SetDefault("Super silk!");
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 100;
+
+        }
+
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            Item.createWall = ModContent.WallType<BrokenCarraige>();
+        }
+    }
+
+    internal class BrokenCarraige : DecorativeWall
+    {
+        public override void SetStaticDefaults()
+        {
+            
+            base.SetStaticDefaults();
+            StructureColor = Color.Gray;
+            //If you need other static defaults it go here
+        }
+    }
 }
