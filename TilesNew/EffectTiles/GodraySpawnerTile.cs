b77653018da27f21d6c1f9e6f28e05257f011b1b
Scolon <scoloned@gmail.com>
2024-12-23 13:11:32 -0500

    godray and cloud color stuff

--- TilesNew/EffectTiles/GodraySpawnerTile.cs
+++ TilesNew/EffectTiles/GodraySpawnerTile.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using System;
 using Terraria;
 using Terraria.ModLoader;
 using Urdveil.Common.Foggy;
 using Urdveil.Common.Shaders;
+using Urdveil.Dusts;
 using Urdveil.Helpers;
 using Urdveil.Tiles;
 
@@ -25,10 +27,54 @@ namespace Urdveil.TilesNew.EffectTiles
     internal class GodraySpawnerWall : FogSpawnerWall
     {
         public override string Texture => this.PathHere() + "/FogSpawnerWall";
+        private float DayProgress
+        {
+            get
+            {
+                if (Main.dayTime)
+                {
+                    float dayProgress = (float)Main.time / (float)Main.dayLength;
+                    return dayProgress;
+                } else if (!Main.dayTime)
+                {
+                    float dayProgress = (float)Main.time / (float)Main.nightLength;
+                    return dayProgress;
+                }
+                else
+                {
+                    return 0f;
+                }
+          
+            }
+        }
+        private Color LightColor
+        {
+            get
+            {
+
+                Color lightColor;
+                if (Main.dayTime)
+                {
+                    Color startColor = Color.Lerp(Color.Purple, Color.White, DayProgress * 2);
+                    Color endColor = Color.Lerp(Color.White, Color.OrangeRed, DayProgress);
+                    lightColor = Color.Lerp(startColor, endColor, DayProgress);
+                    lightColor *= 0.5f;
+                }
+                else
+                {
+                    Color startColor = Color.Lerp(Color.White, Color.White, DayProgress);
+                    Color endColor = Color.Lerp(Color.White, Color.White, DayProgress);
+                    lightColor = Color.Lerp(startColor, endColor, DayProgress);
+                    lightColor *= 0.24f;
+                }
+
+                return lightColor;
+            }
+        }
         public override void FogCreateFunction(Fog fog)
         {
-            fog.blendState = BlendState.Additive;
-            fog.texture = ModContent.Request<Texture2D>("Urdveil/Assets/NoiseTextures/Godray");
+            fog.blendState = BlendState.AlphaBlend;
+            fog.texture = ModContent.Request<Texture2D>("Urdveil/Assets/NoiseTextures/Godray"); 
             fog.startColor = Color.Transparent;
             fog.startScale = new Vector2(Main.rand.NextFloat(0.35f, 1.0f), Main.rand.NextFloat(0.3f, 0.9f)) * 3.5f;
             fog.pulseWidth = Main.rand.NextFloat(0.96f, 0.98f);
@@ -39,37 +85,33 @@ namespace Urdveil.TilesNew.EffectTiles
         public override void FogUpdateFunction(Fog fog)
         {
             base.FogUpdateFunction(fog);
-            float dayProgress = (float)Main.time / (float)Main.dayLength;
-            Color lightColor;
-            if (Main.dayTime)
-            {
-                Color startColor = Color.Lerp(Color.Purple, Color.White, dayProgress);
-                Color endColor = Color.Lerp(Color.White, Color.OrangeRed, dayProgress);
-                lightColor = Color.Lerp(startColor, endColor, dayProgress);
-            }
-            else
-            {
-                Color startColor = Color.Lerp(Color.White, Color.White, dayProgress);
-                Color endColor = Color.Lerp(Color.White, Color.White, dayProgress);
-                lightColor = Color.Lerp(startColor, endColor, dayProgress);
-                lightColor *= 0.24f;
-            }
-
-            Color color = Color.Lerp(Color.Transparent, lightColor, VectorHelper.Osc(0.6f, 1f)) * 0.5f;
+            float offset = fog.position.X + fog.position.Y;
+            offset *= 10;
+            Color color = Color.Lerp(Color.Transparent, LightColor, VectorHelper.Osc(0.6f, 1f, offset: offset)) * 0.5f;
+            color *= MathF.Sin(offset + Main.GlobalTimeWrappedHourly);
             fog.startColor = Color.Lerp(fog.startColor, color, 0.1f);
-            fog.rotation = MathHelper.Lerp(0, MathHelper.ToRadians(165), dayProgress) - MathHelper.ToRadians(45);
+            fog.rotation = MathHelper.Lerp(0, MathHelper.ToRadians(165), DayProgress) - MathHelper.ToRadians(45);
+
+
+           if(DayProgress > 0.95f)
+            {
+                float fadeProgress = (DayProgress - 0.95f) / 0.05f;
+                fadeProgress = 1f - fadeProgress;
+                fog.startColor *= fadeProgress;
+            } 
+            else if (DayProgress < 0.05f)
+            {
+                float fadeProgress = DayProgress / 0.05f;
+                fog.startColor *= fadeProgress;
+            }
         }
 
         public override BaseShader FogShaderFunction()
         {
-            var fogShader = Fog2Shader.Instance;
-            fogShader.FogTexture = ModContent.Request<Texture2D>("Urdveil/Assets/NoiseTextures/Godray");
-            fogShader.ProgressPower = 0.75f;
-            fogShader.EdgePower = 1f;
-            fogShader.Speed = 0f;
-            //      fogShader.Offset = Vector2.Zero;
-            fogShader.Apply();
-            return null;
+            var shader = GodrayShader.Instance;
+            shader.GlowColor = LightColor;
+            shader.Apply();
+            return shader;
         }
     }
 }
