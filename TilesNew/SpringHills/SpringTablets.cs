5b4883324d63d5dfc35b954ff56375d4cb53b937
Scolon <scoloned@gmail.com>
2024-12-23 19:41:04 -0500

    some stuff

---
+++ TilesNew/SpringHills/SpringTablets.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.GameContent.Creative;
+using Terraria.ModLoader;
+using Urdveil.Tiles;
+
+namespace Urdveil.TilesNew.SpringHills
+{
+    public class SpringTabletItem : DecorativeWallItem
+    {
+        public override void SetStaticDefaults()
+        {
+            // Tooltip.SetDefault("Super silk!");
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 100;
+
+        }
+
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            Item.createWall = ModContent.WallType<SpringTablet>();
+        }
+    }
+    internal class SpringTablet : DecorativeWall
+    {
+        public override void SetStaticDefaults()
+        {
+            base.SetStaticDefaults();
+        }
+    }
+}
