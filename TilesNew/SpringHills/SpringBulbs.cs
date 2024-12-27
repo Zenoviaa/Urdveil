5a8eba4cdc1ce912eb9b6bf2b17f77e3ca2fa6ae
Zenovia <calamityjewelstudios@gmail.com>
2024-12-27 14:59:39 -0500

    gru

---
+++ TilesNew/SpringHills/SpringBulbs.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.GameContent.Creative;
+using Terraria.ModLoader;
+using Urdveil.Tiles;
+
+namespace Urdveil.TilesNew.SpringHills
+{
+    public class HangingBulbSmallItem : DecorativeWallItem
+    {
+        public override void SetStaticDefaults()
+        {
+            // Tooltip.SetDefault("Super silk!");
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 100;
+
+        }
+
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            Item.createWall = ModContent.WallType<HangingBulbSmall>();
+        }
+    }
+    internal class HangingBulbSmall : DecorativeWall
+    {
+        public override void SetStaticDefaults()
+        {
+            base.SetStaticDefaults();
+            Origin = DrawOrigin.TopDown;
+            //idk
+            WindSwayOffset = 0f;
+
+            //The max it can sway
+            WindSwayMagnitude = 0.2f;
+
+            //How fast it sways
+            WindSwaySpeed = 0.02f;
+        }
+    }
+
+    public class HangingBulbLongItem : DecorativeWallItem
+    {
+        public override void SetStaticDefaults()
+        {
+            // Tooltip.SetDefault("Super silk!");
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 100;
+
+        }
+
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            Item.createWall = ModContent.WallType<HangingBulbLong>();
+        }
+    }
+    internal class HangingBulbLong : DecorativeWall
+    {
+        public override void SetStaticDefaults()
+        {
+            base.SetStaticDefaults();
+            Origin = DrawOrigin.TopDown;
+            //idk
+            WindSwayOffset = 0f;
+
+            //The max it can sway
+            WindSwayMagnitude = 0.2f;
+
+            //How fast it sways
+            WindSwaySpeed = 0.02f;
+        }
+    }
+
+    public class HangingBulbLargeItem : DecorativeWallItem
+    {
+        public override void SetStaticDefaults()
+        {
+            // Tooltip.SetDefault("Super silk!");
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 100;
+        }
+
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            Item.createWall = ModContent.WallType<HangingBulbLarge>();
+        }
+    }
+
+    internal class HangingBulbLarge : DecorativeWall
+    {
+        public override void SetStaticDefaults()
+        {
+            base.SetStaticDefaults();
+            Origin = DrawOrigin.TopDown;
+            //idk
+            WindSwayOffset = 0f;
+
+            //The max it can sway
+            WindSwayMagnitude = 0.2f;
+
+            //How fast it sways
+            WindSwaySpeed = 0.02f;
+        }
+    }
+
+   
+    
+ 
+   
+}
